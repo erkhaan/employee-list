b82b446a57ab66c3368988006e831a03e95ad98b
erkhaan <notykt@gmail.com>
2024-05-11 12:06:18 +0500

    [add] project init

---
+++ employee-list/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  employee-list
+//
+//  Created by Erkhaan  on 11.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
