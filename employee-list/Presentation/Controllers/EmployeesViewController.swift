110a9df3dfa7509ef62de36e2c6ecd4ca2ef8313
erkhaan <notykt@gmail.com>
2024-05-12 00:33:19 +0500

    [add] unfinished: settings view controller

--- employee-list/Presentation/Controllers/EmployeesViewController.swift
+++ employee-list/Presentation/Controllers/EmployeesViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class EmployeesViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
--- employee-list/Presentation/Controllers/SettingsViewController.swift
+++ employee-list/Presentation/Controllers/SettingsViewController.swift
@@ -6,25 +6,80 @@
 //
 
 import UIKit
+import SnapKit
 
-final class SettingsViewController: UIViewController {
-
+final class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  var tableView: UITableView!
+  let crashCellIdentifier = "CrashCell"
+  let modeCellIdentifier = "ModeCell"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    let button = UIButton(type: .roundedRect)
-    button.frame = CGRect(x: 100, y: 50, width: 100, height: 30)
-    button.setTitle("Test Crash", for: [])
-    button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-    view.addSubview(button)
+    setupViews()
+  }
+  // MARK: - Setup UI
+  
+  private func setupViews() {
+    view.backgroundColor = .white
+    setupNavigationBar()
+    setupTableView()
   }
   
-  @objc func threeDotsButtonTapped() {
-    let viewControllerToOpen = SettingsViewController()
-    navigationController?.pushViewController(viewControllerToOpen, animated: true)
+  private func setupTableView() {
+    tableView = UITableView(frame: view.bounds, style: .grouped)
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: crashCellIdentifier)
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: modeCellIdentifier)
+    view.addSubview(tableView)
   }
   
-  @IBAction func crashButtonTapped(_ sender: AnyObject) {
-      let numbers = [0]
-      let _ = numbers[1]
+  private func setupNavigationBar() {
+    navigationItem.title = "Settings"
+  }
+  
+  // MARK: - UITableViewDataSource Methods
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return section == 0 ? 1 : 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    if indexPath.section == 0 {
+      let cell = tableView.dequeueReusableCell(withIdentifier: crashCellIdentifier, for: indexPath)
+      cell.textLabel?.text = "Crash Application"
+      return cell
+    } else {
+      let cell = tableView.dequeueReusableCell(withIdentifier: modeCellIdentifier, for: indexPath)
+      let modeSwitch = UISwitch()
+      modeSwitch.addTarget(self, action: #selector(modeSwitchValueChanged), for: .valueChanged)
+      cell.accessoryView = modeSwitch
+      cell.textLabel?.text = "Dark Mode"
+      return cell
+    }
+  }
+  
+  // MARK: - UITableViewDelegate Methods
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    if indexPath.section == 0 {
+      crashButtonTapped()
+    }
+  }
+  // MARK: - Actions
+  
+  @objc private func modeSwitchValueChanged(sender: UISwitch) {
+    // Toggle between light and dark mode based on switch state
+  }
+  
+  private func crashButtonTapped() {
+    let numbers = [0]
+    let _ = numbers[1]
   }
 }
