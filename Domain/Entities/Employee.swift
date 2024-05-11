673646e927a33c33e9ef6b93932b6e83dfcc5b0e
erkhaan <notykt@gmail.com>
2024-05-12 00:41:55 +0500

    [add] domain layer

---
+++ Domain/Entities/Employee.swift
@@ -0,0 +1,16 @@
+//
+//  Employee.swift
+//  Domain
+//
+//  Created by Erkhaan  on 12.05.2024.
+//
+
+import Foundation
+
+public final class Employee {
+  let id: Int
+  
+  public init(id: Int) {
+    self.id = id
+  }
+}
