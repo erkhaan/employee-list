036bff133afdf8d7b54feda576c53995853b703e
erkhaan <notykt@gmail.com>
2024-05-12 23:45:14 +0500

    [add] data unit tests

--- DataTests/DataTests.swift
+++ DataTests/DataTests.swift
@@ -7,30 +7,39 @@
 
 import XCTest
 @testable import DataLayer
+import Domain
+import RxSwift
 
 final class DataTests: XCTestCase {
   
+  var themeRepository: ThemeDataRepository!
+  var disposeBag: DisposeBag!
+  
   override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    themeRepository = ThemeDataRepository()
+    disposeBag = DisposeBag()
   }
   
   override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    themeRepository = nil
   }
   
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    // Any test you write for XCTest can be annotated as throws and async.
-    // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-    // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+  func testInitialTheme() throws {
+    let themeType = themeRepository.getTheme
+    XCTAssertEqual(themeType, ThemeType.LIGHT, "Initial theme should be LIGHT")
   }
   
-  func testPerformanceExample() throws {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
+  func testSetTheme() {
+      let newThemeType: ThemeType = .DARK
+      
+      var isDarkThemeResult = false
+      themeRepository.isDarkTheme.subscribe(onNext: { isDark in
+          isDarkThemeResult = isDark
+      }).disposed(by: disposeBag)
+      
+      themeRepository.setTheme(with: newThemeType)
+      
+      XCTAssertEqual(themeRepository.getTheme, newThemeType, "Theme type should be updated to \(newThemeType)")
+      XCTAssertTrue(isDarkThemeResult, "isDarkTheme should be true for DARK theme")
   }
-  
 }
